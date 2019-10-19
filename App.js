a7db7dbc72a4aa6eaca5e2fb568320f6d83084bd
JiaruFu <43692362+JiaruFu@users.noreply.github.com>
2019-10-19 10:38:28 -0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { StyleSheet, Text, View,Button, Image} from 'react-native';
+import AddButton from './components/addButton';
+
+export default function App() {
+  
+  
+
+  return (
+     
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        <Text>Hello Vivian!</Text>
+      </View>
+    
+  )
+
+
+
+
+}
+
+
+
+
